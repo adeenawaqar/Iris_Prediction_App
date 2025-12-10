d91ecbae1187753847f14d0d5be590bd5998683a
AdeenaWaqar <adeenawaqar05@gmail.com>
2025-12-10 10:18:46 -0800

    Update app.py

--- app.py
+++ app.py
@@ -11,13 +11,14 @@ with open('label_encoder.pkl', 'rb') as f:
 st.title("🌸Iris Flower Species Prediction🌸")
 
 st.sidebar.header("Input Features")
-sepal_length = st.sidebar.number_input("Sepal Length (cm)", min_value=0.0, max_value=10.0, value=5.0)
-sepal_width = st.sidebar.number_input("Sepal Width (cm)", min_value=0.0, max_value=10.0, value=3.5)
-petal_length = st.sidebar.number_input("Petal Length (cm)", min_value=0.0, max_value=10.0, value=1.4)
-petal_width = st.sidebar.number_input("Petal Width (cm)", min_value=0.0, max_value=10.0, value=0.2)
+sepal_length = st.sidebar.number_input("Sepal Length (cm)", 0.0, 10.0, 5.0)
+sepal_width = st.sidebar.number_input("Sepal Width (cm)", 0.0, 10.0, 3.5)
+petal_length = st.sidebar.number_input("Petal Length (cm)", 0.0, 10.0, 1.4)
+petal_width = st.sidebar.number_input("Petal Width (cm)", 0.0, 10.0, 0.2)
 
 input_data = pd.DataFrame([[sepal_length, sepal_width, petal_length, petal_width]],
                           columns=['sepal_length', 'sepal_width', 'petal_length', 'petal_width'])
+
 if st.button("Predict"):
     pred = knn.predict(input_data)
     species = le.inverse_transform(pred)[0]
